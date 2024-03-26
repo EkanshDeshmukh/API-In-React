a3f62d1e607ccb1ac4c5fc00381960a6acfc390c
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-03-26 14:11:18 +0530

    Understanding the axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [product, setProduct] = useState([])
+
+  const getProducts = () => {
+    const api = "https://fakestoreapi.com/products"
+    axios.get(api)
+      .then(product => {
+        console.log(product);
+        setProduct(product.data)
+      })
+      .catch(err => { console.log(err) })
+  }
+  const addProducts = () => {
+    const api = 'https://fakestoreapi.com/products'
+    axios.post(api, {
+      title: 'test product',
+      price: 13.5,
+      description: 'lorem ipsum set',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic'
+    }).then(res => console.log(res))
+      .catch(err => console.log(err))
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='p-[5%]'>
+      <button onClick={getProducts} className='bg-red-300 px-4 rounded py-3'>Call API Products</button>
+      <br /><br />
+      <button onClick={addProducts} className='bg-red-300 px-4 rounded py-3'>All Products API</button>
+      <hr className='mt-10' />
+      <ul>
+        {product.length > 0 ? product.map(p => <li key={p.id} className='rounded  mt-5 p-5 bg-red-200' >{p.title}</li>) : <h1>Loading ....</h1>}
+      </ul>
+    </div>
   )
 }
 
